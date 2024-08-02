7cb30ed7c07b0036a22347d464ef067ed2f245e4
Nambi <nambirajan@mosip.io>
2024-08-02 16:35:13 +0530

    UnRAF ID changes to import

--- pre-registration/pre-registration-application-service/src/main/java/io/mosip/preregistration/application/service/DemographicService.java
+++ pre-registration/pre-registration-application-service/src/main/java/io/mosip/preregistration/application/service/DemographicService.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.LinkedHashMap;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;

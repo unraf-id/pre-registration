7ff8df67ae69af5f261758424a3bf69ec4167d96
Nambi <nambirajan@mosip.io>
2024-08-02 17:48:19 +0530

    UnRAF ID changes to import

--- pre-registration/pre-registration-application-service/src/main/java/io/mosip/preregistration/application/service/DemographicService.java
+++ pre-registration/pre-registration-application-service/src/main/java/io/mosip/preregistration/application/service/DemographicService.java
@@ -384,24 +384,17 @@ public class DemographicService implements DemographicServiceIntf {
 	}
 
 	private String getUnrafId(JSONObject jsonObject, String key) {
-		JSONObject identity = getJSONObject(jsonObject, "identity");
+		JSONObject identity = jsonObject.get("identity");
 		if (identity == null) {
 			return null;
 		}
-		JSONObject unrafId = getJSONObject(jsonObject, key);
+		JSONObject unrafId = identity.get(key);
 		log.info("sessionId", "idType", "id",
 				"UnrafId : " + unrafId);
 		if (unrafId == null) {
 			return null;
 		}
 		return unrafId.toString();
-	}
-
-	private JSONObject getJSONObject(JSONObject jsonObject, String key) {
-		if(jsonObject == null)
-			return null;
-		LinkedHashMap identity = (LinkedHashMap) jsonObject.get(key);
-		return identity != null ? new JSONObject(identity) : null;
 	}
 
 	/*
